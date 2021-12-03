d7694bbed6ce1fda8c4e210db7b2583e1f97927b
RameshRawat <pradiprawal2057@gmail.com>
2021-12-03 09:41:31 +0545

    change

---
+++ src/App.js
@@ -0,0 +1,7 @@
+
+// const App=()=>{
+//   const currDate=new Date().toDateString();
+//   const currTime=new Date().toLocaleTimeString();
+
+// }
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
+import './App.css';
 
-  let curDate=new Date(2021,3,10,19);
+  let curDate=new Date();
   curDate=curDate.getHours();
   let greeting=""; 
+  const cssStyle={};
 
   if(curDate>=1 && curDate<12){
     greeting="Good morning";
+    cssStyle.color="green";
 
   }else{
     if(curDate>=12 && curDate<=19){
-      greeting="good afternoon";
+      greeting="Good afternoon";
+      cssStyle.color="red";
     }
     else{
-      greeting="good night";
+      greeting="Good night";
+      cssStyle.color="black";
     }
   }
+  
+    const currDate=new Date().toDateString();
+    const currTime=new Date().toLocaleTimeString();
+  
+  
 
 ReactDOM.render(
     <>
-      <h1>Hello sir,{greeting}</h1>
+    <h1>Hello sir,</h1><span style={cssStyle}>{greeting} <h3>{currDate}and {currTime}</h3></span>
     </>,
   document.getElementById('root')
 );
